1d95fb8f27f3b99807fa32bd4531b611e409f2b9
kyhol <Kyle.Hollett@keyin.com>
2024-06-20 09:33:42 -0230

    Banner added to order page

--- brad_kyle_brian_sprint1/Scripts/home-page.js
+++ brad_kyle_brian_sprint1/Scripts/home-page.js
@@ -8,28 +8,28 @@ var index = 0;
 show_image(index);
 
 function show_image(i) {
-  index += i;
+    index += i;
 
-  var images = document.getElementsByClassName("foodImage");
-  var dots = document.getElementsByClassName("dot");
+    var images = document.getElementsByClassName("foodImage");
+    var dots = document.getElementsByClassName("dot");
+    
+    for (var j = 0; j < images.length; j++) {
+        images[j].style.display = "none";
+    }
 
-  for (var j = 0; j < images.length; j++) {
-    images[j].style.display = "none";
-  }
+    for (var j = 0; j < dots.length; j++) {
+        dots[j].className = dots[j].className.replace(" active", "");
+    }
 
-  for (var j = 0; j < dots.length; j++) {
-    dots[j].className = dots[j].className.replace(" active", "");
-  }
+    if (index > images.length - 1) {
+        index = 0;
+    }
+    if (index < 0) {
+        index = images.length - 1;
+    }
 
-  if (index > images.length - 1) {
-    index = 0;
-  }
-  if (index < 0) {
-    index = images.length - 1;
-  }
-
-  images[index].style.display = "flex";
-  dots[index].className += " active";
+    images[index].style.display = "flex";
+    dots[index].className += " active";
 }
 var index1 = 0;
 var index2 = 0;
@@ -37,23 +37,19 @@ var index2 = 0;
 showImage('review1', index1);
 
 function showImage(carouselId, i) {
-  if (carouselId === "review1") {
-    index1 += i;
-    var images = document
-      .getElementById("slider-container1")
-      .getElementsByClassName("reviewPics");
-    var dots = document
-      .getElementById("slider-container1")
-      .getElementsByClassName("dot");
-  }
+    if (carouselId === 'review1') {
+        index1 += i;
+        var images = document.getElementById('slider-container1').getElementsByClassName("reviewPics");
+        var dots = document.getElementById('slider-container1').getElementsByClassName("dot");
+    }
 
-  for (var j = 0; j < images.length; j++) {
-    images[j].style.display = "none";
-  }
+    for (var j = 0; j < images.length; j++) {
+        images[j].style.display = "none";
+    }
 
-  for (var j = 0; j < dots.length; j++) {
-    dots[j].className = dots[j].className.replace(" active", "");
-  }
+    for (var j = 0; j < dots.length; j++) {
+        dots[j].className = dots[j].className.replace(" active", "");
+    }
 
     if (carouselId === 'review1' && index1 > images.length - 1) {
         index1 = 0;
