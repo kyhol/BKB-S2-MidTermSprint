91a0b34797229a80413b2cc1985008f750eb5b6f
Brian Janes <janes.brian@gmail.com>
2024-06-25 10:29:17 -0230

    removed parenthesis error

--- brad_kyle_brian_sprint1/Scripts/script.js
+++ brad_kyle_brian_sprint1/Scripts/script.js
@@ -1,16 +1,14 @@
+// Banner begins being fixed and hovering over the top of the page when the user scrolls.
+document.addEventListener("DOMContentLoaded", function () {
+  const banner = document.querySelector(".bannertop");
+  const bannerOffsetTop = banner.offsetTop;
 
-// Banner begins being fixed and hovering over the top of the page when the user scrolls. 
-document.addEventListener('DOMContentLoaded', function() {
-    const banner = document.querySelector('.bannertop');
-    const bannerOffsetTop = banner.offsetTop;
-  
-    window.addEventListener('scroll', function() {
-      if (window.scrollY > bannerOffsetTop) {
-        banner.classList.add('fixed');
-      } else {
-        banner.classList.remove('fixed');
-      }
-    });
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > bannerOffsetTop) {
+      banner.classList.add("fixed");
+    } else {
+      banner.classList.remove("fixed");
+    }
   });
 });
 
