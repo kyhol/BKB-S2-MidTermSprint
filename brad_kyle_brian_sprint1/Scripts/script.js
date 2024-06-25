677e38c7f9626c0b800d60c23f5001a9c0d22ab2
kyhol <Kyle.Hollett@keyin.com>
2024-06-25 08:31:58 -0230

    More comments

--- brad_kyle_brian_sprint1/Scripts/script.js
+++ brad_kyle_brian_sprint1/Scripts/script.js
@@ -1,3 +1,4 @@
+// Banner begins being fixed and hovering over the top of the page when the user scrolls. 
 document.addEventListener('DOMContentLoaded', function() {
     const banner = document.querySelector('.bannertop');
     const bannerOffsetTop = banner.offsetTop;
