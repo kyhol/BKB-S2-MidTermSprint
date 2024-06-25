a847dc13f53401df5e8a118b58a33fe907157e74
Brian Janes <janes.brian@gmail.com>
2024-06-25 15:14:53 -0230

    fixed sm error

--- brad_kyle_brian_sprint1/Scripts/home-page.js
+++ brad_kyle_brian_sprint1/Scripts/home-page.js
@@ -11,170 +11,176 @@ show_image(index); // Function to display the image carousel on the home page. w
 function show_image(i) {
   index += i;
 
-
-function showImage(carouselId, i) { //Image carousel function copied and edited from above, serves the same purpose but I decided not to use the dots on this one.
-    if (carouselId === 'review1') {
-        index1 += i;
-        var images = document.getElementById('slider-container1').getElementsByClassName("reviewPics");
-        var dots = document.getElementById('slider-container1').getElementsByClassName("dot");
+  function showImage(carouselId, i) {
+    //Image carousel function copied and edited from above, serves the same purpose but I decided not to use the dots on this one.
+    if (carouselId === "review1") {
+      index1 += i;
+      var images = document
+        .getElementById("slider-container1")
+        .getElementsByClassName("reviewPics");
+      var dots = document
+        .getElementById("slider-container1")
+        .getElementsByClassName("dot");
     }
 
+    for (var j = 0; j < images.length; j++) {
+      images[j].style.display = "none";
+    }
 
-  for (var j = 0; j < images.length; j++) {
-    images[j].style.display = "none";
+    for (var j = 0; j < dots.length; j++) {
+      dots[j].className = dots[j].className.replace(" active", "");
+    }
+
+    if (index > images.length - 1) {
+      index = 0;
+    }
+    if (index < 0) {
+      index = images.length - 1;
+    }
+
+    images[index].style.display = "flex";
+    dots[index].className += " active";
+  }
+  var index1 = 0;
+  var index2 = 0;
+
+  showImage("review1", index1);
+
+  function showImage(carouselId, i) {
+    //Image carousel function copied and edited from above.
+    if (carouselId === "review1") {
+      index1 += i;
+      var images = document
+        .getElementById("slider-container1")
+        .getElementsByClassName("reviewPics");
+      var dots = document
+        .getElementById("slider-container1")
+        .getElementsByClassName("dot");
+    }
+
+    for (var j = 0; j < images.length; j++) {
+      images[j].style.display = "none";
+    }
+
+    for (var j = 0; j < dots.length; j++) {
+      dots[j].className = dots[j].className.replace(" active", "");
+    }
+
+    if (carouselId === "review1" && index1 > images.length - 1) {
+      index1 = 0;
+    }
+    if (carouselId === "review1" && index1 < 0) {
+      index1 = images.length - 1;
+    }
+
+    images[carouselId === "review1" ? index1 : index2].style.display = "flex";
   }
 
-  for (var j = 0; j < dots.length; j++) {
-    dots[j].className = dots[j].className.replace(" active", "");
-  }
+  document.addEventListener("DOMContentLoaded", function () {
+    // display list of hours of operation, address, contact, and details.
+    fetch("../Data/restaurant_data.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Response failed");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Data fetched successfully:", data); // Log the fetched data
 
-  if (index > images.length - 1) {
-    index = 0;
-  }
-  if (index < 0) {
-    index = images.length - 1;
-  }
+        const restaurant = data.restaurant;
+        const addressElement = document.getElementById("address");
+        const contactElement = document.getElementById("contact");
+        const hoursOfOperationElement =
+          document.getElementById("hours_of_operation");
+        const detailsElement = document.getElementById("details");
 
-  images[index].style.display = "flex";
-  dots[index].className += " active";
-}
-var index1 = 0;
-var index2 = 0;
+        // Display the address
+        const address = restaurant.address;
+        addressElement.textContent = `${address.street}, ${address.city}, ${address.province}, ${address.country}, ${address.postal_code}`;
 
-showImage("review1", index1);
+        // Display the contact
+        contactElement.textContent = `Phone: ${restaurant.contact.phone}`;
 
-function showImage(carouselId, i) {
-  //Image carousel function copied and edited from above.
-  if (carouselId === "review1") {
-    index1 += i;
-    var images = document
-      .getElementById("slider-container1")
-      .getElementsByClassName("reviewPics");
-    var dots = document
-      .getElementById("slider-container1")
-      .getElementsByClassName("dot");
-  }
+        // Display the hours of operation
+        const hoursOfOperation = restaurant.hours_of_operation;
+        for (const day in hoursOfOperation) {
+          const openHours = hoursOfOperation[day].open;
+          const closeHours = hoursOfOperation[day].close;
 
-  for (var j = 0; j < images.length; j++) {
-    images[j].style.display = "none";
-  }
+          let hoursText;
+          if (Array.isArray(openHours)) {
+            hoursText = `${day}: ${openHours[0]} - ${closeHours[0]}, ${openHours[1]} - ${closeHours[1]}`;
+          } else {
+            hoursText = `${day}: ${openHours} - ${closeHours}`;
+          }
 
-  for (var j = 0; j < dots.length; j++) {
-    dots[j].className = dots[j].className.replace(" active", "");
-  }
-
-  if (carouselId === "review1" && index1 > images.length - 1) {
-    index1 = 0;
-  }
-  if (carouselId === "review1" && index1 < 0) {
-    index1 = images.length - 1;
-  }
-
-  images[carouselId === "review1" ? index1 : index2].style.display = "flex";
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  // display list of hours of operation, address, contact, and details.
-  fetch("../Data/restaurant_data.json")
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Response failed");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log("Data fetched successfully:", data); // Log the fetched data
-
-      const restaurant = data.restaurant;
-      const addressElement = document.getElementById("address");
-      const contactElement = document.getElementById("contact");
-      const hoursOfOperationElement =
-        document.getElementById("hours_of_operation");
-      const detailsElement = document.getElementById("details");
-
-      // Display the address
-      const address = restaurant.address;
-      addressElement.textContent = `${address.street}, ${address.city}, ${address.province}, ${address.country}, ${address.postal_code}`;
-
-      // Display the contact
-      contactElement.textContent = `Phone: ${restaurant.contact.phone}`;
-
-      // Display the hours of operation
-      const hoursOfOperation = restaurant.hours_of_operation;
-      for (const day in hoursOfOperation) {
-        const openHours = hoursOfOperation[day].open;
-        const closeHours = hoursOfOperation[day].close;
-
-        let hoursText;
-        if (Array.isArray(openHours)) {
-          hoursText = `${day}: ${openHours[0]} - ${closeHours[0]}, ${openHours[1]} - ${closeHours[1]}`;
-        } else {
-          hoursText = `${day}: ${openHours} - ${closeHours}`;
+          const listItem = document.createElement("li");
+          listItem.textContent = hoursText;
+          hoursOfOperationElement.appendChild(listItem);
         }
 
-        const listItem = document.createElement("li");
-        listItem.textContent = hoursText;
-        hoursOfOperationElement.appendChild(listItem);
-      }
-
-      // Display the details
-      const details = restaurant.details.services;
-      details.forEach((detail) => {
-        const listItem = document.createElement("li");
-        listItem.textContent = detail;
-        detailsElement.appendChild(listItem);
-      });
-    })
-    .catch((error) => console.error("Error fetching data:", error));
-});
-
-// fetch('../Data/restaurant_data.json')
-//     .then(response => {
-//         if (!response.ok) {
-//             throw new Error('Response failed');
-//         }
-//         return response.json();
-//     })
-//     .then(data => {
-//         console.log('Data fetched successfully:', data); // Log the fetched data
-
-//         // Your existing code for displaying the JSON data
-//     })
-//     .catch(error => console.error('Error fetching data:', error)); // Log any errors that occur during the fetch
-
-document.addEventListener("DOMContentLoaded", () => {
-  //function used in conjunction with CSS to animate the letters starting on the left and going through to the right at .2s intervals
-  const letters = document.querySelectorAll(".letter");
-  letters.forEach((letter, index) => {
-    setTimeout(() => {
-      letter.classList.add("animate");
-    }, index * 200);
+        // Display the details
+        const details = restaurant.details.services;
+        details.forEach((detail) => {
+          const listItem = document.createElement("li");
+          listItem.textContent = detail;
+          detailsElement.appendChild(listItem);
+        });
+      })
+      .catch((error) => console.error("Error fetching data:", error));
   });
-});
 
-//Cycles through 3 images in the deliverMenuContainer div every 5 seconds.
-document.addEventListener("DOMContentLoaded", function() {
-    const deliverMenuContainer = document.querySelector('.deliverMenuContainer');
-    const image = document.createElement('img');  // Create an img element
-    deliverMenuContainer.appendChild(image);  // Append the img element to the container
+  // fetch('../Data/restaurant_data.json')
+  //     .then(response => {
+  //         if (!response.ok) {
+  //             throw new Error('Response failed');
+  //         }
+  //         return response.json();
+  //     })
+  //     .then(data => {
+  //         console.log('Data fetched successfully:', data); // Log the fetched data
+
+  //         // Your existing code for displaying the JSON data
+  //     })
+  //     .catch(error => console.error('Error fetching data:', error)); // Log any errors that occur during the fetch
+
+  document.addEventListener("DOMContentLoaded", () => {
+    //function used in conjunction with CSS to animate the letters starting on the left and going through to the right at .2s intervals
+    const letters = document.querySelectorAll(".letter");
+    letters.forEach((letter, index) => {
+      setTimeout(() => {
+        letter.classList.add("animate");
+      }, index * 200);
+    });
+  });
+
+  //Cycles through 3 images in the deliverMenuContainer div every 5 seconds.
+  document.addEventListener("DOMContentLoaded", function () {
+    const deliverMenuContainer = document.querySelector(
+      ".deliverMenuContainer"
+    );
+    const image = document.createElement("img"); // Create an img element
+    deliverMenuContainer.appendChild(image); // Append the img element to the container
 
     const imagePaths = [
-        '../Images/MenuAd/MenuAd.svg', 
-        '../Images/MenuAd/MenuAd2.svg', 
-        '../Images/MenuAd/MenuAd3.svg'
+      "../Images/MenuAd/MenuAd.svg",
+      "../Images/MenuAd/MenuAd2.svg",
+      "../Images/MenuAd/MenuAd3.svg",
     ];
     let currentIndex = 0;
 
     function showNextImage() {
-        image.classList.remove('active'); // Remove the active class to start the fade-out transition
+      image.classList.remove("active"); // Remove the active class to start the fade-out transition
 
-        setTimeout(() => {
-            image.src = imagePaths[currentIndex]; // Change the image source
-            image.classList.add('active'); // Add the active class to start the fade-in transition
-            currentIndex = (currentIndex + 1) % imagePaths.length; // Wrap around using modulo
-        }, 1000); // Wait for the fade-out transition to complete
+      setTimeout(() => {
+        image.src = imagePaths[currentIndex]; // Change the image source
+        image.classList.add("active"); // Add the active class to start the fade-in transition
+        currentIndex = (currentIndex + 1) % imagePaths.length; // Wrap around using modulo
+      }, 1000); // Wait for the fade-out transition to complete
     }
 
     setInterval(showNextImage, 5000);
     showNextImage(); // Show the first image immediately
-});
+  });
+}
